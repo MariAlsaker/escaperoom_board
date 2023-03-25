21c40cd5782f212ebd54506ee4b2001b5eea52a4
fsaev <fredrik@saevland.com>
2023-03-25 23:22:39 +0100

    Add proper states to lcd

--- include/lcd.h
+++ include/lcd.h
@@ -3,11 +3,10 @@
 
 enum lcd_states
 {
-    L_STARTUP,
-    L_INFO,
-    L_INFO_SNEEZE,
-    L_INFO_SNEEZE_THANKS,
-    L_MENU //Future
+    L_IDLE,
+    L_INPUT,
+    L_ERROR,
+    L_SUCCESS
 };
 
 void init_lcd(void);
--- src/lcd.cpp
+++ src/lcd.cpp
@@ -5,7 +5,7 @@
 
 static LiquidCrystal* lcd;
 
-static lcd_states lcd_state = L_STARTUP;
+static lcd_states lcd_state = L_IDLE;
 static uint32_t last_update;
 static uint32_t transition_timer;
 
@@ -24,29 +24,33 @@ void tick_lcd(void)
         last_update = millis();
         switch(lcd_state)
         {
-            case L_STARTUP:
-                lcd_state = L_INFO;
-                break;
-            case L_INFO:
+            case L_IDLE:
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("ME&T");
+                lcd->print("ERR: 0x3d21");
                 lcd->setCursor(0, 1);
-                lcd->print("Sneezeman");
+                lcd->print("...");
                 break;
-            case L_INFO_SNEEZE:
+            case L_INPUT:
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("Sneezing...");
+                lcd->print("Ready for input:");
+                lcd->setCursor(0, 1);
+                lcd->print("x");
                 break;
-            case L_INFO_SNEEZE_THANKS:
+            case L_ERROR:
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("Enjoy sneeze!");
-                if(millis() - transition_timer > 1000){
-                    lcd_state = L_INFO;
+                lcd->print("ERR: WRONG INPUT");
+                break;
+            case L_SUCCESS:
+                lcd->clear();
+                lcd->setCursor(0, 0);
+                lcd->print("INPUT ACCEPTED!");
+                if(millis() - transition_timer > 3000){
+                    lcd_state = L_INPUT;
                 }
-                break;
+            
         }
     }
 }
