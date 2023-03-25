7cfa903fe3ae43d31e541ac1ed74952f7de83f7d
fsaev <fredrik@saevland.com>
2023-03-25 23:23:44 +0100

    Clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,10 @@ void setup() {
 
   Serial.begin(9600);
 
-  // pinMode(LOCK_A_PIN, OUTPUT);
-  // pinMode(LOCK_B_PIN, OUTPUT);
-  // pinMode(LOCK_C_PIN, OUTPUT);
-  // pinMode(LOCK_D_PIN, OUTPUT);
-  // pinMode(LOCK_E_PIN, OUTPUT);
-
   pinMode(AO_STATE_PIN, INPUT_PULLUP);
   pinMode(CO_STATE_PIN, INPUT_PULLUP);
   pinMode(PT_STATE_PIN, INPUT_PULLUP);
   pinMode(BL_STATE_PIN, INPUT_PULLUP);
-
-  // pinMode(SOUND_GOOD_TP, OUTPUT);
-  // pinMode(SOUND_BAD_TP, OUTPUT);
-  // pinMode(SOUND_WIN_TP, OUTPUT);
-  // pinMode(SOUND_RES_1_TP, OUTPUT);
-  // pinMode(SOUND_RES_2_TP, OUTPUT);
-  // pinMode(SOUND_RES_3_TP, OUTPUT);
-  // pinMode(SOUND_RES_4_TP, OUTPUT);
-  // pinMode(SOUND_RES_5_TP, OUTPUT);
 
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
@@ -41,13 +26,8 @@ void setup() {
 
   pinMode(BUZZER_PIN, OUTPUT);
 
-  pinMode(LEDSTRIP_PIN, OUTPUT);
-
-  //init_com();
   init_gamestate();
-  // init_lockcontrol();
   init_input();
-  //init_sound();
   init_ledcontrol();
   //init_lcd();
   init_ble();
@@ -59,7 +39,6 @@ void loop() {
   // Heartbeat
     if(millis() - subroutine_timestamp > 100){
       subroutine_timestamp = millis();
-      digitalWrite(13, !digitalRead(13));
     }
 
   tick_gamestate();
@@ -68,6 +47,5 @@ void loop() {
   // tick_sound();
   tick_leds();
   tick_ble();
-  //tick_com();
   //tick_lcd();
 }
