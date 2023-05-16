7f420acedfd19448f8bcf4d48b9d45314d9ac430
Fredrik Sævland <fredrik.savland@laerdal.com>
2023-05-16 15:33:12 +0200

    Refactor game state

--- include/gamestate.h
+++ include/gamestate.h
@@ -2,8 +2,10 @@
 #define GAMESTATE_H
 
 #define B_TIMEOUT_MS 5000
-#define D_TIMEOUT_MS 5000
-#define E_TIMEOUT_MS 5000
+#define D_TIMEOUT_MS 10000
+#define E_TIMEOUT_MS 10000
+
+#define SUCCESS_TIME 2000
 
 #define D_GCS_RANGE_LOWER 150
 #define D_GCS_RANGER_UPPER 200
@@ -15,8 +17,10 @@ enum gamestates
     GS_B_TIMEOUT,
     GS_C, // Bleeding
     GS_D, // Consciousness (keypad)
+    GS_D_SUCCESS,
     GS_D_TIMEOUT,
     GS_E, // Exposure (keypad)
+    GS_E_SUCCESS,
     GS_E_TIMEOUT,
     GS_FINISHED
 };
@@ -29,6 +33,9 @@ enum handler_returns
 };
 
 void init_gamestate(void);
+uint8_t input_char_gamestate(char input);
+uint8_t request_verification(void);
+void wipe_char_gamestate(void);
 void tick_gamestate(void);
 
 #endif
--- src/gamestate.cpp
+++ src/gamestate.cpp
@@ -2,23 +2,61 @@
 #include "gamestate.h"
 #include "defines.h"
 #include "cmd.h"
+#include "lcd.h"
+#include "led_control.h"
+
+uint32_t timestamp_debug = 0;
+char debug_text[128];
 
 enum gamestates gamestate = GS_A;
-
 uint32_t timestamp = 0;
 uint32_t sync_timestamp = 0;
 uint8_t sync_payload[8];
 
+char* numberbuf;
+uint8_t input_accepted;
+
+uint8_t inputver_requested = 0;
 
 void init_gamestate(void){
     timestamp = millis();
-    sync_timestamp = millis();
+    numberbuf = get_numberbufstart();
+    input_accepted = 0;
+}
 
-    memset(sync_payload, 0, 8);
-} 
+uint8_t input_char_gamestate(char input)
+{
+    if(!input_accepted){ return 0; }
+
+    if(numberbuf != get_numberbufstart() + 8){
+        *numberbuf = input;
+        numberbuf++;
+        *numberbuf = '\0'; //Overflowing is expected
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void wipe_char_gamestate(void)
+{
+    if(!input_accepted){ return; }
+
+    numberbuf = get_numberbufstart();
+    memset(numberbuf, ' ', sizeof(char) * 8);
+}
+
+uint8_t request_verification(void)
+{
+    if(!input_accepted){ return 0; }
+
+    inputver_requested = 1;
+    return 1;
+}
 
 // Airway Handler
 static inline handler_returns handler_a(void){
+    set_state_lcd(L_IDLE);
     if(!digitalRead(AO_STATE_PIN)){
         return RET_DONE;
     }else{
@@ -28,6 +66,7 @@ static inline handler_returns handler_a(void){
 
 // Breathing Handler
 static inline handler_returns handler_b(void){
+    set_state_lcd(L_IDLE);
     if(!digitalRead(PT_STATE_PIN_ERROR)){
         return RET_ERROR;
     }
@@ -41,6 +80,7 @@ static inline handler_returns handler_b(void){
 
 // Circulation Handler
 static inline handler_returns handler_c(void){
+    set_state_lcd(L_IDLE);
     if(!digitalRead(BL_STATE_PIN)){
         return RET_DONE;
     }else{
@@ -50,23 +90,54 @@ static inline handler_returns handler_c(void){
 
 // Disability Handler
 static inline handler_returns handler_d(void){
-    if(!digitalRead(CO_STATE_PIN)){
-        return RET_DONE;
-    }else{
-        return RET_INPROGRESS;
+    set_state_lcd(L_INPUT_D);
+    input_accepted = 1;
+    if(inputver_requested){
+        inputver_requested = 0;
+        char* input = get_numberbufstart();
+        Serial.println(input);
+        if(!strncmp(input, "333", sizeof(char) * 8)){
+            wipe_char_gamestate();
+            return RET_DONE;
+        }else{
+            wipe_char_gamestate();
+            return RET_ERROR;
+        }
     }
+    return RET_INPROGRESS;
+
 }
 
 // Exposure Handler
 static inline handler_returns handler_e(void){
-    if(!digitalRead(CO_STATE_PIN)){
-        return RET_DONE;
-    }else{
-        return RET_INPROGRESS;
+    set_state_lcd(L_INPUT_E);
+    input_accepted = 1;
+    if(inputver_requested){
+        inputver_requested = 0;
+        char* input = get_numberbufstart();
+        Serial.println(input);
+        if(!strncmp(input, "111", sizeof(char) * 8)){
+            wipe_char_gamestate();
+            return RET_DONE;
+        }else{
+            wipe_char_gamestate();
+            return RET_ERROR;
+        }
     }
+    return RET_INPROGRESS;
 }
 
 void tick_gamestate(void){
+
+    if(millis() - timestamp_debug > 500){
+        timestamp_debug = millis();
+        snprintf(debug_text, 128 * sizeof(char), "(GS: %d) [%d, %d, %d, %d]",
+            gamestate,
+            !digitalRead(AO_STATE_PIN), !digitalRead(PT_STATE_PIN), 
+            !digitalRead(PT_STATE_PIN_ERROR), !digitalRead(BL_STATE_PIN));
+        Serial.println(debug_text);
+    }
+
     handler_returns ret;
     switch(gamestate){
         //Airway
@@ -106,16 +177,32 @@ void tick_gamestate(void){
         case GS_D:
             sync_payload[3] = IN_PROGRESS;
             ret = handler_d();
+            set_ledmode(LED_G, LED_OFF);
+            set_ledmode(LED_R, LED_OFF);
             if(ret == RET_ERROR){
                 timestamp = millis();
                 gamestate = GS_D_TIMEOUT;
             }else if(ret == RET_DONE){
                 sync_payload[3] = SOLVED;
+                timestamp = millis();
+                gamestate = GS_D_SUCCESS;
+            }
+            break;
+        //Disability success time
+        case GS_D_SUCCESS:
+            set_state_lcd(L_SUCCESS);
+            set_ledmode(LED_G, LED_ON);
+            set_ledmode(LED_R, LED_OFF);
+            if(millis() - timestamp > SUCCESS_TIME){
+                timestamp = millis();
                 gamestate = GS_E;
             }
             break;
         //Disability Timeout
         case GS_D_TIMEOUT:
+            set_state_lcd(L_ERROR);
+            set_ledmode(LED_G, LED_OFF);
+            set_ledmode(LED_R, LED_PULSE);
             sync_payload[3] = TIMEOUT;
             if(millis() - timestamp > D_TIMEOUT_MS){
                 gamestate = GS_D;
@@ -125,16 +212,32 @@ void tick_gamestate(void){
         case GS_E:
             sync_payload[4] = IN_PROGRESS;
             ret = handler_e();
+            set_ledmode(LED_G, LED_OFF);
+            set_ledmode(LED_R, LED_OFF);
             if(ret == RET_ERROR){
                 timestamp = millis();
-                gamestate = GS_D_TIMEOUT;
+                gamestate = GS_E_TIMEOUT;
             }else if(ret == RET_DONE){
                 sync_payload[4] = SOLVED;
+                timestamp = millis();
+                gamestate = GS_E_SUCCESS;
+            }
+            break;
+        //Exposure success time
+        case GS_E_SUCCESS:
+            set_state_lcd(L_SUCCESS);
+            set_ledmode(LED_G, LED_ON);
+            set_ledmode(LED_R, LED_OFF);
+            if(millis() - timestamp > SUCCESS_TIME){
+                timestamp = millis();
                 gamestate = GS_FINISHED;
             }
             break;
         //Exposure Timeout
         case GS_E_TIMEOUT:
+            set_state_lcd(L_ERROR);
+            set_ledmode(LED_G, LED_OFF);
+            set_ledmode(LED_R, LED_PULSE);
             sync_payload[4] = TIMEOUT;
             if(millis() - timestamp > E_TIMEOUT_MS){
                 gamestate = GS_E;
@@ -142,6 +245,9 @@ void tick_gamestate(void){
             break;
         //Finish
         case GS_FINISHED:
+            set_state_lcd(L_FINISH);
+            set_ledmode(LED_G, LED_PULSE);
+            set_ledmode(LED_R, LED_OFF);
             break;
     }
 }
