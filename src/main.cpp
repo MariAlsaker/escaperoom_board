900d4e567feb04a874ab371a22b0d6da3bcf4910
Fredrik Sævland <fredrik.savland@laerdal.com>
2023-05-16 15:34:07 +0200

    Update main to new system

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,20 @@ uint32_t subroutine_timestamp = 0;
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(AO_STATE_PIN, INPUT_PULLUP);
-  pinMode(CO_STATE_PIN, INPUT_PULLUP);
   pinMode(PT_STATE_PIN, INPUT_PULLUP);
+  pinMode(PT_STATE_PIN_ERROR, INPUT_PULLUP);
   pinMode(BL_STATE_PIN, INPUT_PULLUP);
 
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-
-  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(REDLED_PIN, OUTPUT);
+  pinMode(GREENLED_PIN, OUTPUT);
 
   init_gamestate();
   init_input();
   init_ledcontrol();
-  //init_lcd();
+  init_lcd();
   init_ble();
 
   subroutine_timestamp = millis();
@@ -39,10 +36,8 @@ void loop() {
     }
 
   tick_gamestate();
-  // tick_locks();
-  //tick_input();
-  // tick_sound();
+  tick_input();
+  tick_lcd();
   tick_leds();
   tick_ble();
-  //tick_lcd();
 }
