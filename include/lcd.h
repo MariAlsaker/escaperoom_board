b90f8df04610ea9eb49b1f321af76981f1f2b5e8
Fredrik Sævland <fredrik.savland@laerdal.com>
2023-05-16 15:33:25 +0200

    Refactor LCD

--- include/lcd.h
+++ include/lcd.h
@@ -1,15 +1,21 @@
 #ifndef LCD_H
 #define LCD_H
 
+#include <stdint.h>
+
 enum lcd_states
 {
     L_IDLE,
-    L_INPUT,
+    L_INPUT_D,
+    L_INPUT_E,
     L_ERROR,
-    L_SUCCESS
+    L_SUCCESS,
+    L_FINISH
 };
 
 void init_lcd(void);
+void set_state_lcd(lcd_states newstate);
+char* get_numberbufstart(void);
 void tick_lcd(void);
 
 
--- src/lcd.cpp
+++ src/lcd.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <iostream>
 #include "lcd.h"
 #include "defines.h"
 
@@ -7,16 +8,25 @@ static LiquidCrystal* lcd;
 
 static lcd_states lcd_state = L_IDLE;
 static uint32_t last_update;
-static uint32_t transition_timer;
+static uint32_t blink_timestamp = 0;
+static uint8_t blink_state = 1;
+
+char textbuffer[11] = {'>', ' ', ' ',' ',' ',' ',' ',' ',' ','\0'};
 
 #define LCD_UPDATE_INTERVAL 100
 
 void init_lcd(void)
 {
-    lcd = new LiquidCrystal(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+    lcd = new LiquidCrystal(LCD_RS, LCD_RW, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
     lcd->begin(16, 2);
     last_update = millis();
+    blink_timestamp = millis();
+    lcd_state = L_IDLE;
 }
+
+char* get_numberbufstart(void){ return &textbuffer[2]; }
+
+void set_state_lcd(lcd_states newstate){ lcd_state = newstate; }
 
 void tick_lcd(void)
 {
@@ -26,30 +36,59 @@ void tick_lcd(void)
         {
             case L_IDLE:
                 lcd->clear();
+               if(millis() - blink_timestamp > 500){
+                    blink_timestamp = millis();
+                    blink_state = !blink_state;
+                }
+                if(blink_state){
                 lcd->setCursor(0, 0);
-                lcd->print("ERR: 0x3d21");
+                lcd->print("ERR DEVICE");
                 lcd->setCursor(0, 1);
-                lcd->print("...");
+                lcd->print("MALFUNCTION");
+                }
                 break;
-            case L_INPUT:
+            case L_INPUT_D:
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("Ready for input:");
+                lcd->print("0x0D INPUT:");
                 lcd->setCursor(0, 1);
-                lcd->print("x");
+                lcd->print(textbuffer);
+                break;
+            case L_INPUT_E:
+                lcd->clear();
+                lcd->setCursor(0, 0);
+                lcd->print("0x0E INPUT:");
+                lcd->setCursor(0, 1);
+                lcd->print(textbuffer);
                 break;
             case L_ERROR:
+               if(millis() - blink_timestamp > 500){
+                    blink_timestamp = millis();
+                    blink_state = !blink_state;
+                }
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("ERR: WRONG INPUT");
+                lcd->print("ERR 0x1824:");
+                lcd->setCursor(0, 1);
+                if(blink_state){
+                    lcd->print("WRONG INPUT!");
+                }else{
+                    lcd->print("            ");
+                }
                 break;
             case L_SUCCESS:
                 lcd->clear();
                 lcd->setCursor(0, 0);
-                lcd->print("INPUT ACCEPTED!");
-                if(millis() - transition_timer > 3000){
-                    lcd_state = L_INPUT;
-                }
+                lcd->print("INPUT ACCEPTED");
+                lcd->setCursor(0, 1);
+                lcd->print("...");
+                break;
+            case L_FINISH:
+                lcd->clear();
+                lcd->setCursor(0, 0);
+                lcd->print("SYSTEM ONLINE");
+                lcd->setCursor(0, 1);
+                lcd->print("STATUS 0xF721");
             
         }
     }
